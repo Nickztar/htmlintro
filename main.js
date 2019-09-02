d841e21d74d62e375d744b91972da85d991cfd34
Nicke <Nicholas0012@gmail.com>
2019-09-02 14:20:33 +0200

    Initial

---
+++ main.js
@@ -0,0 +1,20 @@
+console.log( document.getElementsByTagName("nav"));
+
+
+let menuButton = _id("menuButton");
+
+menuButton.addEventListener("click", function(){
+    
+    let nav = _tagFirst("nav");
+    nav.classList.toggle("hidden");
+});
+
+
+// wideHelpers
+function _id(id){
+    return document.getElementById(id);
+}
+function _tagFirst(tag)
+{
+    return document.getElementsByTagName(tag)[0];
+}
